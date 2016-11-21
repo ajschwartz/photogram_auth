ce86cdf3fdc0ed4a795d1f0f3907852b10cdb153
Andrew Schwartz <schwartz.andrew10@gmail.com>
2016-11-20 21:16:03 -0600

    complete through #17

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render("users/show.html.erb")
+  end
+
+  def my_likes
+    @user = current_user
+
+    render("users/my_likes.html.erb")
+  end
+
+end
---
+++ app/models/photo.rb
@@ -0,0 +1,12 @@
+class Photo < ActiveRecord::Base
+
+validates :user_id, :presence => true
+
+has_many :likes
+has_many :comments
+
+belongs_to :user
+
+has_many :fans, :through => :likes, :source => :user
+
+end
